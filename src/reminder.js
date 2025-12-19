f29eadcfe826a0af00324ab5e53c821a4ac86e43
Kuria300 <eugenekuria66@gmail.com>
2025-12-19 20:22:38 +0300

    changes

--- src/reminder.js
+++ src/reminder.js
@@ -10,10 +10,19 @@
  * Example Usage:
  * delayedReminder("Time to stretch!", 3000).then(() => console.log("Reminder done."));
  */
+
 function delayedReminder(message, delay) {
   // Return a promise
   // Use setTimeout to log the message after the specified delay
   // Resolve the promise once the message is logged
+  return new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      console.log(message)
+      resolve()
+    }, delay)
+  })
 }
+
+//delayedReminder('Time to Win', 3000).then(()=>{console.log('reminder was logged')})
 
 module.exports = { delayedReminder };
